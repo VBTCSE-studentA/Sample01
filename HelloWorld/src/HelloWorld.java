8e2f10fda39d59f39f0876dd538a382f7454f8c2
J. M. Hinckley <jhinckley@vbisd.org>
2018-01-11 09:49:24 -0500

    Updated HelloWorld.java
    Added two new lines to output text.  This is only to demonstrate how to incorporate your local changes into the group project, making them available to your collaborators.

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,6 +4,10 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world!");
+		
+		// Some things added by me to the team project.
+		System.out.println("Here is my contribution...");
+		System.out.println("Actually, I am adding a second, bonus line.");
 	}
 
 }
